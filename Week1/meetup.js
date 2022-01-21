d67c96f1a4b2bb09a1c6ccadb4f787bbd193e5ca
Fikret Yilmaz <yilmz.fikret@gmail.com>
2022-01-21 21:50:54 +0100

    Fikret Yilmaz Database Week 1 Update

--- Week1/meetup.js
+++ Week1/meetup.js
@@ -8,35 +8,111 @@ const connection = mysql.createConnection({
 });
 
 connection.connect();
-// TODO Insert value to invite table
-const insert_value_invite =
-  "insert into invite (invitee_no, invitee_name, invitee_by) VALUES (01,'Fikret','Ali'),(02,'Emine','Ali'),(03,'Umut','Ahmet'),(04,'Bera','Mehmet'),(05,'Hatice','Ali')";
-connection.query(insert_value_invite, function (error, results, fields) {
+
+const dropDatabase = 'DROP DATABASE IF EXISTS meetup';
+connection.query(dropDatabase, (error, results, fields) => {
   if (error) {
     throw error;
   }
-  console.log('The value of invite added:');
 });
-// TODO Insert value to meeting table
-const insert_value_meeting =
-  "insert into meeting (meeting_no, meeting_title,starting_time, ending_time, room_no) VALUES (01,'Project','2021-01-01','2021-01-02',21),(02,'Eating','2021-02-01','2021-02-02',22),(03,'Meeting','2021-01-04','2021-01-04',24),(04,'Movie','2021-01-01','2021-01-02',26),(05,'Sales','2021=05-01','2021-05-02',21)";
 
-connection.query(insert_value_meeting, function (error, results, fields) {
+const createDatabase = 'CREATE DATABASE meetup';
+connection.query(createDatabase, (error, results, fields) => {
   if (error) {
     throw error;
   }
-  console.log('The value of meeting added:');
 });
 
-// TODO Insert value to room table
-const insert_value_room =
-  "insert into room (room_no, room_name,floor_number) VALUES (11,'Project',1),(22,'Eating',2),(24,'Meeting',4),(26,'Movie',2),(21,'Sales',2)";
-
-connection.query(insert_value_room, function (error, results, fields) {
+const connectMeetup = 'USE meetup';
+connection.query(connectMeetup, (error, results, fields) => {
   if (error) {
     throw error;
   }
-  console.log('The value of room added:');
 });
+const createTableInvitee =
+  'CREATE TABLE Invite (invitee_no INT, invitee_name VARCHAR(50), invitee_by VARCHAR(50))';
+connection.query(createTableInvitee, (error, results, fields) => {
+  if (error) {
+    throw error;
+  }
+});
+
+const createTableRoom =
+  'CREATE TABLE Room (room_no SMALLINT, room_name VARCHAR(50), floor_number SMALLINT)';
+connection.query(createTableRoom, (error, results, fields) => {
+  if (error) {
+    throw error;
+  }
+});
+
+const createTableMeeting =
+  'CREATE TABLE Meeting (meeting_no INT, meeting_title TEXT, starting_time DATETIME, ending_time DATETIME, room_no SMALLINT)';
+connection.query(createTableMeeting, (error, results, fields) => {
+  if (error) {
+    throw error;
+  }
+});
+
+const insertValueInvite =
+  'INSERT INTO Invite (invitee_no, invitee_name, invitee_by) VALUES ?';
+const inviteValues = [
+  [01, 'Fikret', 'Ali'],
+  [02, 'Emine', 'Ali'],
+  [03, 'Umut', 'Ahmet'],
+  [04, 'Bera', 'Mehmet'],
+  [05, 'Hatice', 'Ali'],
+];
+connection.query(
+  insertValueInvite,
+  [inviteValues],
+  function (error, results, fields) {
+    if (error) {
+      throw error;
+    }
+    console.log('The value of invite added');
+  },
+);
+
+const insertValueMeeting =
+  'INSERT INTO meeting (meeting_no, meeting_title,starting_time, ending_time, room_no) VALUES ?';
+const meetingValues = [
+  [01, 'Project', '2021-01-01 12:30:00', '2021-01-02 13:00;00', 21],
+  [02, 'Eating', '2021-02-01 13:00:00', '2021-02-02 14:00:00', 22],
+  [03, 'Meeting', '2021-01-04 14:30:00', '2021-01-04 15:00:00', 24],
+  [04, 'Movie', '2021-01-01 11:30:00', '2021-01-02 11:50:00', 26],
+  [05, 'Sales', '2021=05-01 10:00:00', '2021-05-02 11:00:00', 21],
+];
+
+connection.query(
+  insertValueMeeting,
+  [meetingValues],
+  function (error, results, fields) {
+    if (error) {
+      throw error;
+    }
+    console.log('The value of meeting added');
+  },
+);
+
+const insertValueRoom =
+  'INSERT INTO Room (room_no, room_name,floor_number) VALUES ?';
+const roomValues = [
+  [11, 'Project', 1],
+  [22, 'Eating', 2],
+  [24, 'Meeting', 4],
+  [26, 'Movie', 2],
+  [21, 'Sales', 2],
+];
+
+connection.query(
+  insertValueRoom,
+  [roomValues],
+  function (error, results, fields) {
+    if (error) {
+      throw error;
+    }
+    console.log('The value of room added');
+  },
+);
 
 connection.end();
--- Week1/world.js
+++ Week1/world.js
@@ -1,6 +1,4 @@
-const express = require('express');
 const mysql = require('mysql');
-const app = express();
 
 const connection = mysql.createConnection({
   host: 'localhost',
@@ -9,7 +7,6 @@ const connection = mysql.createConnection({
   database: 'world',
 });
 
-//Connect to world database
 connection.connect((err) => {
   if (err) {
     throw error;
@@ -17,10 +14,9 @@ connection.connect((err) => {
   console.log('Mysql Connected');
 });
 
-//TODO 1) Names of the country with population grater than 8 million
-let select_names =
+const selectNames =
   'SELECT Name,Population FROM country WHERE Population >8000000 ORDER BY Population ASC';
-connection.query(select_names, function (error, results, fields) {
+connection.query(selectNames, function (error, results, fields) {
   if (error) {
     throw error;
   }
@@ -28,12 +24,10 @@ connection.query(select_names, function (error, results, fields) {
     'Names of the country with population grater than 8 million:',
     results,
   );
-  //console.table(results);
 });
 
-//TODO 2) The names of countries that have “land” in their names
-let country_with_land = 'SELECT Name FROM country WHERE Name LIKE "%land%"';
-connection.query(country_with_land, function (error, results, fields) {
+const countryWithLand = 'SELECT Name FROM country WHERE Name LIKE "%land%"';
+connection.query(countryWithLand, function (error, results, fields) {
   if (error) {
     throw error;
   }
@@ -41,13 +35,11 @@ connection.query(country_with_land, function (error, results, fields) {
     'The names of countries that have “land” in their names:',
     results,
   );
-  //console.table(results);
 });
 
-//TODO 3) The names of the cities with population in between 500,000 and 1 million
-let city_with_population =
+const cityWithPopulation =
   'SELECT Name,Population FROM city WHERE Population BETWEEN 500000 AND 1000000 ORDER BY Population ASC';
-connection.query(city_with_population, function (error, results, fields) {
+connection.query(cityWithPopulation, function (error, results, fields) {
   if (error) {
     throw error;
   }
@@ -55,21 +47,17 @@ connection.query(city_with_population, function (error, results, fields) {
     'The names of the cities with population in between 500,000 and 1 million:',
     results,
   );
-  //console.table(results);
 });
 
-//TODO 4) the name of all the countries on the continent ‘Europe’
-let europe_countries = 'SELECT Name FROM country WHERE Continent = "Europe"';
-connection.query(europe_countries, function (error, results, fields) {
+const europeCountries = 'SELECT Name FROM country WHERE Continent = "Europe"';
+connection.query(europeCountries, function (error, results, fields) {
   if (error) {
     throw error;
   }
   console.log('The name of all the countries on the continent Europe', results);
-  //console.table(results);
 });
 
-//TODO 5) List all the countries in the descending order of their surface areas
-let countries_surfaces =
+const countries_surfaces =
   'SELECT Name,SurfaceArea FROM country ORDER BY  SurfaceArea DESC';
 connection.query(countries_surfaces, function (error, results, fields) {
   if (error) {
@@ -79,73 +67,53 @@ connection.query(countries_surfaces, function (error, results, fields) {
     'All the countries in the descending order of their surface areas',
     results,
   );
-  //console.table(results);
 });
 
-//TODO 6) The names of all the cities in the Netherlands
-let cities_in_netherlands = 'SELECT Name FROM city WHERE CountryCode = "NLD"';
-connection.query(cities_in_netherlands, function (error, results, fields) {
+const citiesInNetherlands = 'SELECT Name FROM city WHERE CountryCode = "NLD"';
+connection.query(citiesInNetherlands, function (error, results, fields) {
   if (error) {
     throw error;
   }
   console.log('The names of all the cities in the Netherlands', results);
-  //console.table(results);
 });
 
-//TODO 7) The population of Rotterdam
-
-let population_of_rotterdam =
+const populationOfRotterdam =
   'SELECT Population FROM city WHERE Name = "Rotterdam"';
-connection.query(population_of_rotterdam, function (error, results, fields) {
+connection.query(populationOfRotterdam, function (error, results, fields) {
   if (error) {
     throw error;
   }
   console.log('The population of Rotterdam is:', results);
-  //console.table(results);
 });
 
-//TODO 8)  The top 10 countries by Surface Area
-let top_10_countries_by_surfaceArea =
+const top10CountriesBySurfaceArea =
   'SELECT Name FROM country ORDER BY SurfaceArea DESC LIMIT 10';
 connection.query(
-  top_10_countries_by_surfaceArea,
+  top10CountriesBySurfaceArea,
   function (error, results, fields) {
     if (error) {
       throw error;
     }
     console.log(' The top 10 countries by Surface Area are:', results);
-    //console.table(results);
   },
 );
 
-//TODO 9) The top 10 most populated cities
-let top_10_countries_by_population =
+const top10CountriesByPopulation =
   'SELECT Name,Population FROM city ORDER BY Population DESC LIMIT 10';
-connection.query(
-  top_10_countries_by_population,
-  function (error, results, fields) {
-    if (error) {
-      throw error;
-    }
-    console.log(' The top 10 most populated cities are:', results);
-    //console.table(results);
-  },
-);
+connection.query(top10CountriesByPopulation, function (error, results, fields) {
+  if (error) {
+    throw error;
+  }
+  console.log(' The top 10 most populated cities are:', results);
+});
 
-//TODO 10) The population number of the world
-
-let population_of_world =
+const populationOfWorld =
   'SELECT SUM(Population) AS WorldPopulation FROM country';
-connection.query(population_of_world, function (error, results, fields) {
+connection.query(populationOfWorld, function (error, results, fields) {
   if (error) {
     throw error;
   }
   console.log(' The population number of the world is:', results);
-  //console.table(results);
 });
 
 connection.end();
-
-app.listen('3000', () => {
-  console.log('Server started on port 3000');
-});
